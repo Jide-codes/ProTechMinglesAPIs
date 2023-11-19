d4854c80a93a27950b0e58574c9510534745c07c
Yusuf Saubana Mayowa <101975553+Crane04@users.noreply.github.com>
2023-11-19 01:24:16 +0100

    add rest framework settings

--- ProTechMingle/settings.py
+++ ProTechMingle/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    "userprofiles",
+    
+    # Dependencies
+    "rest_framework",
+    "rest_framework.authtoken"
 ]
 
 MIDDLEWARE = [
