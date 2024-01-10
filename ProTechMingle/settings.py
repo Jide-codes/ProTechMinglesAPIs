ab2315cf949f2b2304a86c1551019fca2cbc3317
Jide-codes <hezekiaholajide3@gmail.com>
2024-01-09 20:36:57 -1100

    still on the corheaders config

--- ProTechMingle/settings.py
+++ ProTechMingle/settings.py
@@ -24,7 +24,7 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
-ALLOWED_HOSTS = ["https://protechmingles.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["protechmingles.onrender.com"]
 
 CSRF_TRUSTED_ORIGINS=["https://protechmingles.onrender.com"]
 
@@ -101,10 +101,10 @@ SIMPLE_JWT = {
 #     r"^https://\w+\.example\.com$"
 # ]
 
-CORS_ALLOWED_ORIGINS =[
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:5500",
-]
+# CORS_ALLOWED_ORIGINS =[
+#     "http://127.0.0.1:3000",
+#     "http://127.0.0.1:5500",
+# ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
